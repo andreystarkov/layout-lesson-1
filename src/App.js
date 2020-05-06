0a85880165441569a4df8e4c95009eea5d410ace
Andrey Starkov <admstarkov@sberbank.ru>
2020-05-06 06:19:20 +0300

    first blood

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className= "top">Tell us about you</h1>
+          <div className="row">
+            <input type="text" placeholder="Search"/>
+          </div>
+          <div className="row half">
+            <input type="password" placeholder="Search"/>
+            <input type="text" placeholder="Search" />
+          </div>
+          <div className="row">
+            <input type="text" placeholder="Search"/>
+          </div>
+      </div>
     </div>
   );
 }
